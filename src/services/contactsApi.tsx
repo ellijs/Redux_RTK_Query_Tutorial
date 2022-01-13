acc5752a60124e33c9a87aa4a77650248f488b13
Jessica (Ji Sun) Lee <79885174+ellijs@users.noreply.github.com>
2022-01-12 23:13:17 -0500

    Update contactsApi.tsx

--- src/services/contactsApi.tsx
+++ src/services/contactsApi.tsx
@@ -6,34 +6,43 @@ export const contactsApi = createApi({
     reducerPath: "contactsApi",
     
     baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3006/" }),
+    
+    // auto-refetch based on tagtypes
+    tagTypes: ['Contact'], // type can be string, obj or callback
+    
     endpoints:(builder) => ({
         // Since we don't pass any parameter, we pass down 'void'
         contacts: builder.query<Contact[], void>({
-            query: () => '/contacts'
+            query: () => '/contacts',
+            provideTags: ['Contact'].  // we need to provide tags if we need to use query
         }),
         contact: builder.query<Contact, string>({
-            query: (id) => `/contacts/${id}`
+            query: (id) => `/contacts/${id}`,
+            provideTags: ['Contact']
         }),
         addContact: builder.mutation<void, Contact>({ 
             query: contact => ({ 
                 url: '/contacts',
                 method: "POST",
                 body: contact
-            })
+            }),
+            invalidatesTags: ['Contact']  // but for the mutation we need to give INVALID tags
         }),
         updateContact: builder.mutation<void, Contact>({ 
             query: ({ id, ...rest }) => ({ 
                 url: `/contacts/${id}`,
                 method: "PUT",
                 body: rest
-            })
+            }),
+            invalidatesTags: ['Contact'] 
         }),
         // we just need ID to pass for deletion
         deleteContact: builder.mutation<void, string>({ 
             query: (id ) => ({ 
                 url: `/contacts/${id}`,
                 method: "DELETE",
-            })
+            }),
+            invalidatesTags: ['Contact'] 
         }),
     })
 })
